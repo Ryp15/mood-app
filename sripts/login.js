71cc75542a8b876111bd6a70ae67c5241f4b1839
Ryp15 <164873229+Ryp15@users.noreply.github.com>
2025-12-19 21:49:59 +0100

    firstCommit

---
+++ sripts/login.js
@@ -0,0 +1,8 @@
+function showOrhidePassword(){
+    if (document.getElementById('show-hidePassword').textContent=='show'){
+        document.getElementById('show-hidePassword').textContent='hide';
+    }
+    else{
+        document.getElementById('show-hidePassword').textContent='show';
+    }
+}
